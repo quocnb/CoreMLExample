9d7170f2260d8c75e69e834942244ff6157b82c7
quocnb <quocnb.vnu@gmail.com>
2017-12-27 16:13:17 +0900

    Add hat by Vision

--- CoreMLExample/CoreMLExample/Views/AnnotationView.swift
+++ CoreMLExample/CoreMLExample/Views/AnnotationView.swift
@@ -24,15 +24,33 @@ class AnnotationView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        drawFacesBounds()
+        faces.forEach { (faceDimension) in
+            self.drawBound(faceRect: faceDimension.boundRect)
+            self.drawHat(faceRect: faceDimension.boundRect)
+        }
     }
 
-    private func drawFacesBounds() {
-        self.faces.forEach { (face) in
-            let path = UIBezierPath(rect: face.boundRect)
-            self.color.setStroke()
-            path.stroke()
-        }
+    private func drawBound(faceRect: CGRect) {
+        let path = UIBezierPath(rect: faceRect)
+        self.color.setStroke()
+        path.stroke()
+    }
+
+    private func drawHat(faceRect: CGRect) {
+        let hat = #imageLiteral(resourceName: "hat")
+        let hatSize = hat.size
+        let headSize = faceRect.size
+        let newHatWidth = 1.5 * headSize.width
+        let hatRatio = newHatWidth / hatSize.width
+
+        let scaleTransform = CGAffineTransform(scaleX: hatRatio, y: hatRatio)
+        let adjustHatsize = hatSize.applying(scaleTransform)
+
+        let hatRect = CGRect(x: faceRect.midX - (adjustHatsize.width / 2.0),
+                             y: faceRect.minY - adjustHatsize.height,
+                             width: adjustHatsize.width,
+                             height: adjustHatsize.height)
+        hat.draw(in: hatRect)
     }
 
 }
